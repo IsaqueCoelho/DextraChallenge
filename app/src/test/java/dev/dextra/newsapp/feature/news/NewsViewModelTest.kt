55a29fd1305196cb28cb737031527c40b0934a4f
Isaque.dias-ext@ab-inbev.com <isaque.dias-ext2@ab-inbev.com>
2020-02-17 10:38:08 -0300

    Implementing Unit Test and Instrumentation Test on NewsActivity

---
+++ app/src/test/java/dev/dextra/newsapp/feature/news/NewsViewModelTest.kt
@@ -0,0 +1,56 @@
+package dev.dextra.newsapp.feature.news
+
+import dev.dextra.newsapp.TestConstants
+import dev.dextra.newsapp.api.model.ArticlesResponse
+import dev.dextra.newsapp.base.BaseTest
+import dev.dextra.newsapp.base.NetworkState
+import dev.dextra.newsapp.base.TestSuite
+import dev.dextra.newsapp.utils.JsonUtils
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.koin.test.get
+
+class NewsViewModelTest : BaseTest(){
+
+    private val emptyResponse = ArticlesResponse(ArrayList(), "ok", 20)
+
+    lateinit var viewModel: NewsViewModel
+
+    @Before
+    fun setupTest() {
+        viewModel = TestSuite.get()
+    }
+
+    @Test
+    fun testGetSources() {
+        viewModel.loadArticlesList(1)
+
+        assert(viewModel.articles.value?.size == 11)
+        assertEquals(NetworkState.SUCCESS, viewModel.networkState.value)
+
+        viewModel.onCleared()
+
+        assert(viewModel.getDisposables().isEmpty())
+    }
+
+    @Test
+    fun testEmptySources() {
+        TestSuite.mock(TestConstants.newsURL).body(JsonUtils.toJson(emptyResponse)).apply()
+
+        viewModel.loadArticlesList(1)
+
+        assert(viewModel.articles.value?.size == 0)
+        assertEquals(NetworkState.EMPTY, viewModel.networkState.value)
+    }
+
+    @Test
+    fun testErrorSources() {
+        TestSuite.mock(TestConstants.newsURL).throwConnectionError().apply()
+
+        viewModel.loadArticlesList(1)
+
+        assert(viewModel.articles.value == null)
+        assertEquals(NetworkState.ERROR, viewModel.networkState.value)
+    }
+}
